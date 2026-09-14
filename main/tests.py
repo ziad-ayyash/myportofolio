4dd0d74bec0b1c72881621c71ed698a2b5589748
M. Ziad Ayyash <ayyash0026@gmail.com>
2026-09-14 18:42:34 +0700

    feat: Unit Test Implementation for 'Projects' Page'

--- main/tests.py
+++ main/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Experience, Project
 
 
 class MainTest(TestCase):
@@ -11,6 +11,14 @@ class MainTest(TestCase):
             title="Asisten Dosen PBP",
             description="Membantu mahasiswa memahami pengembangan web.",
             category="part-time",
+        )
+        self.project = Project.objects.create(
+            title="Portfolio",
+            description="Portfolio website to showcase my work and experience.",
+            thumbnail="img/project_thumbnails/portfolio.png",
+            type="Website",
+            development_status="in_development",
+            maintenance_status="maintained",
         )
 
     def test_main_url_is_accessible(self):
@@ -25,6 +33,8 @@ class MainTest(TestCase):
         response = self.client.get("/halaman-yang-tidak-ada/")
 
         self.assertEqual(response.status_code, 404)
+
+## Experience Page ##
 
     def test_experience_model(self):
         self.assertEqual(str(self.experience), "Asisten Dosen PBP")
@@ -56,3 +66,28 @@ class MainTest(TestCase):
         self.assertFalse(self.experience.is_ongoing)
         self.assertContains(response, "Selesai")
         self.assertNotContains(response, "Sedang berlangsung")
+
+## Projects Page ##
+
+    def test_project_model(self):
+            self.assertEqual(str(self.project), "Portfolio")
+            self.assertEqual(self.project.type, "Website")
+            self.assertTrue(self.project.is_maintained)
+
+    def test_projects_page(self):
+            response = self.client.get(reverse("main:show_projects"))
+    
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, "projects.html")
+            self.assertContains(response, self.project.title)
+            self.assertContains(response, self.project.description)
+            self.assertContains(response, "Website")
+            self.assertContains(response, "Projects")
+            self.assertContains(response, f'href="{reverse("main:show_main")}"')
+            self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+
+    def test_empty_projects_page(self):
+            Project.objects.all().delete()
+            response = self.client.get(reverse("main:show_projects"))
+
+            self.assertContains(response, "No Projects Have Been Added Yet.")
