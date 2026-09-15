d6b183d60bbf7e2b0c52278d7575d13ccf6010ee
M. Ziad Ayyash <ayyash0026@gmail.com>
2026-09-15 19:49:43 +0700

    feat: modified Project Model & forms.py setup

---
+++ main/forms.py
@@ -0,0 +1,52 @@
+from django.forms import ModelForm, TextInput, Textarea, URLInput
+
+from main.models import Project
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            "title",
+            "description",
+            "type",
+            "link",
+            "thumbnail",
+        ]
+
+        labels = {
+            "title": "Project name",
+            "description": "Project description",
+            "type": "Project Type",
+            "link": "URL Proyek",
+            "thumbnail": "URL Gambar Proyek",
+        }
+
+        widgets = {
+            "title": TextInput(
+                attrs={
+                    "placeholder": "Portfolio Website",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Ceritakan Proyekmu",
+                    "rows": 3,
+                }
+            ),
+            "type": TextInput(
+                attrs={
+                    "placeholder": "Jenis Proyekmu",
+                }
+            ),
+            "link": URLInput(
+                attrs={
+                    "placeholder": "https://github.com/kakBurhan/burhanquestv4",
+                }
+            ),
+            "thumbnail": URLInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
+                }
+            ),
+        }
