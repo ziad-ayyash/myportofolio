21028c48ef67a6ae1c4ef855560d506bf443433f
M. Ziad Ayyash <ayyash0026@gmail.com>
2026-09-16 14:01:32 +0700

    fix: Project Cards Now Works and Looks as Intended

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-
+from django.contrib import messages
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from main.models import Experience, Project
+from main.forms import ProjectForm
 
 NAME = "Ziad Ayyash"
 
@@ -29,3 +32,17 @@ def show_projects(request):
             "projects_list": Project.objects.all(),
         }
     return render(request, "projects.html", context)
+
+def create_project(request):
+    form = ProjectForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Proyek baru berhasil ditambahkan!")
+        return redirect("main:show_projects")
+
+    context = {
+        "name": "Ziad",
+        "form": form,
+    }
+    return render(request, "projects_form.html", context)
